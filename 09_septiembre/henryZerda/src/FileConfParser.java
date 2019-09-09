7f0787bf4fbf8c7c319e1943c4437101e9af2c03
henryzrr <henry.zrz@gmail.com>
2019-09-08 21:49:24 -0400

    tarea 09 de septiembre terminado

---
+++ 09_septiembre/henryZerda/src/FileConfParser.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+class FileConfParser {
+    private Set<String> commandList;
+    FileConfParser(Set<String>commandList) {
+        this.commandList=commandList;
+    }
+    List<String> getCommandAndParams(String line)throws Exception{
+        List<String> tokens= new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(line," ");
+        String command = tokenizer.nextToken();
+        if(!commandList.contains(command)){
+            throw new Exception("Comando no existente "+command);
+        }else if(!tokenizer.hasMoreTokens()) {
+            throw new Exception("Error! Faltan valores para " + command);
+        }
+        tokens.add(command);
+        while (tokenizer.hasMoreTokens()){
+            tokens.add(tokenizer.nextToken());
+        }
+        return  tokens;
+    }
+}
