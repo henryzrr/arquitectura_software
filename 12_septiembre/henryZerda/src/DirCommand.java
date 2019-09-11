d4275f45c6411eb1eba74146408e2d2ec4f57916
henryzrr <henry.zrz@gmail.com>
2019-09-10 20:26:09 -0400

    presentacon 12 de septiembre terminada

---
+++ 12_septiembre/henryZerda/src/DirCommand.java
@@ -0,0 +1,48 @@
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class DirCommand implements ICommand {
+    private String dirName;
+    private String dirPath;
+    private List<String> CommandAndParams;
+    private boolean isWindows;
+    private Map<String,String> systemValues;
+
+    DirCommand(List<String> commandAndParamList, boolean isWindowsOS, Map<String, String> systemValues) {
+        CommandAndParams = commandAndParamList;
+        this.isWindows = isWindowsOS;
+        this.systemValues = systemValues;
+    }
+
+    private String getExecutionPath(){
+        return (System.getProperty("user.dir"));
+    }
+
+    @Override
+    public void execute() throws Exception {
+        setDirNameAndPath();
+        systemValues.put(dirName,dirPath);
+    }
+    private void setDirNameAndPath()throws Exception{
+        if(CommandAndParams.size()>2){
+            throw new Exception("Error en  Comando dir, mal parametro");
+        }
+        String dirNameAndPath = CommandAndParams.get(1);
+        StringTokenizer tokenizer = new StringTokenizer(dirNameAndPath,"=");
+        dirName = tokenizer.nextToken();
+        if(!tokenizer.hasMoreElements())
+            if(dirName.equals("home"))
+                dirPath = getExecutionPath();
+            else
+                throw new Exception("Error en  Comando dir, mal parametro "+dirName);
+        else{
+            dirPath = tokenizer.nextToken();
+            dirPath = dirPath.replaceAll("\"","");
+            if(isWindows) {
+                dirPath = systemValues.get("volume") + ":" + dirPath;
+                dirPath = dirPath.replaceAll("/", "\\\\");
+            }
+        }
+    }
+}
