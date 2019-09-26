c5a98df649f4efc598e1732d298353593f263a6f
henryzrr <henry.zrz@gmail.com>
2019-09-26 13:36:10 -0400

    26 de septiembre final

--- 26_septiembre/henryZerda/src/JavaCompiler.java
+++ 26_septiembre/henryZerda/src/JavaCompiler.java
@@ -24,6 +24,7 @@ class JavaCompiler implements ICompiler{
         command.add(file);
         processBuilder.command(command);
         Process process = processBuilder.start();
+        System.out.println(process.info());
         process.waitFor();
     }
 
